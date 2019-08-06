54145b8ad9a5a3c4c27e4804179c7babfaaa1304
Anja Weigelt <aweigelt@ufl.edu>
2019-08-06 00:44:37 -0400

    nav, footer, homepage set-up

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// Index H1 Animation
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: function(el, i) {
+      return 50 * i;
+    }
+  });
